21e21752cc8adce9b9ce52911c040b84f3fcba4d
Daniel Pagharion <dpaghari@gmail.com>
2015-12-05 02:59:57 -0800

    Basic Routing

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+  Route::get('/', 'WelcomeController@index');
+  Route::get('contact', 'WelcomeController@contact');
+  Route::get('home', 'WelcomeController@index');
+  
+  Route::controllers([
+    'auth' => 'Auth\AuthController',
+    'password' => 'Auth\PasswordController'
+]);
